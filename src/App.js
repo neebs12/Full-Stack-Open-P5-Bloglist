cf066f164a884f9382fae79f944288387fc815be
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-06 05:27:45 +1200

    completed exercise 5.1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,78 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+  const handleLogin = async (event) => { // this will be an async cb
+    event.preventDefault()
+    let credentials = { username, password }
+    try {
+      let response = await loginService.login(credentials)
+      // console.log('logging in', response)
+      // now that we have the response (successfully, this is where we get all the relevant blogs)
+      let blogs = await blogService.getUserSpecificBlogs(response)
+      setBlogs(blogs)
+      setUser(response)
+      setUsername('') //clears information
+      setPassword('')
+    } catch (e) {
+      console.error('login unsuccessful', e)
+    }
+  }
+
+  const handleGenericInputChange = hook => { // pfa
+    return event => hook(event.target.value)
+  }
+
+  // these components will be optionally rendered
+  // loginFormComponent for if user === null
+  // loggedInFormComponent for if user !== null
+  const loginFormComponent = () => { 
+    // this will have a form
+    return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input 
+            type="text"
+            value={username}
+            name="Username"
+            onChange={handleGenericInputChange(setUsername)}
+          />
+        </div>
+        <div>
+          password
+          <input 
+            type="text"
+            value={password}
+            name="Password"
+            onChange={handleGenericInputChange(setPassword)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+    )
+  }
+
+  const loggedInComponent = () => {
+    return (<div><p>{user.username} is logged in</p></div>)
+  }
 
   return (
     <div>
       <h2>blogs</h2>
+      {user === null ? 
+        loginFormComponent() : 
+        loggedInComponent()
+      }
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,11 +1,29 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-let exportObj = { getAll } 
+const getUserSpecificBlogs = async (user) => {
+  let username = user.username
+  const response = await axios.get(baseUrl)
+  // then filter response.data information based on `username`
+  // response.data is an array of objects
+  let filteredResponse = response.data.filter(blog => {
+    let specificUser = blog.user.username
+    return specificUser === username
+  })
+  return filteredResponse
+}
+
+let exportObj = { getAll, getUserSpecificBlogs } 
 
 export default exportObj
