46f7bf5b81fc605933ae98661cc8f66a80b5de7d
Jason Aricheta <jason.aricheta@gmail.com>
2022-06-08 05:25:59 +1200

    completed exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,13 @@
+import {useState} from 'react'
+import Toggleable from "./Toggleable"
+
 const Blog = ({blog}) => {
+  // cannot use toggleable component directly 
+  // -- as button does not appear adjacent to blog
+  const [visibility, setVisibility] = useState(false)
+  const displayVisibility = {display: visibility ? '' : 'none'}
+  const buttonName = visibility ? 'view' : 'hide'
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -7,9 +16,23 @@ const Blog = ({blog}) => {
     marginBottom: 5
   }
 
+  const BlogDetails = (props) => {
+    // displays blog.url, blog.likes, blog.user.username
+    return (
+      <div>
+        {blog.url} <br></br>
+        likes {blog.likes} <button>like</button><br></br>
+        {blog.user.username}<br></br>
+      </div>
+    )
+  }
   return (
     <div style={blogStyle}>
-      {blog.title} {blog.author}
+      {blog.title} {blog.author} 
+      <button onClick={() => setVisibility(!visibility)}>{buttonName}</button>
+      <div style={displayVisibility}>
+        <BlogDetails />
+      </div>
     </div>  
   )
 }
